d203d715424f03590b2a9d341c0190b66eebed18
Will Sanzone <wsanzone@gmail.com>
2022-10-10 15:26:54 -0600

    database updates

--- personalwebsite/config.py
+++ personalwebsite/config.py
@@ -8,6 +8,7 @@ class Config:
     
   #  SQLALCHEMY_DATABASE_URI = "sqlite:///site.db"
     MAIL_SERVER = 'smtp.googlemail.com'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE')
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get('EMAILUSERNAME')
--- personalwebsite/models.py
+++ personalwebsite/models.py
@@ -1,12 +1,19 @@
-from datetime import datetime
+#from datetime import datetime
 from personalwebsite import db
 
 class Contact(db.Model):
+    __tablename__ = 'contact'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(25), nullable=False)
     email = db.Column(db.String(120), unique=False, nullable=False)
     subject = db.Column(db.String(40), nullable=False)
     message = db.Column(db.Text, nullable=False)
 
+    def __init__(self, name, email, subject, message):
+        self.name = name
+        self.email = email
+        self.subject = subject
+        self.message = message
+
     def __repr__(self):
-        return f"Contact('{self.name}', '{self.email}')"
+        return f"Event {subject}"
